bfe7317d4741ae51ab685bd3fae6c686b5705f7c
Snus292 <georgiigiblov@gmail.com>
2024-08-05 19:23:45 +0300

    подключение стилей и добавление их классам и тегам

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { createRoot } from 'react-dom/client'
 import App from "./App"
+import "./css/style.css"
 
 const app = createRoot(document.getElementById("app"))
 // const app = ReactDOMClient.createRoot(document.getElementById("app"))
