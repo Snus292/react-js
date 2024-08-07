3c4848765f24bc26e7c0e30073e0028a8a1ce67d
Snus292 <georgiigiblov@gmail.com>
2024-08-07 14:55:14 +0300

    реализован конструктор состояний и обработчик onChange c функцией установки полученого события  в компоненте addUser.js и в app.js метод addUser вывода и добавления данных на страницу на страницу

--- src/App.js
+++ src/App.js
@@ -6,20 +6,67 @@ import AddUser from "./components/addUser"
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    title: "Inception",
+                    releaseYear: 2010,
+                    plot: "A thief who enters the dreams of others to steal their secrets.",
+                    genres: ["Action", "Sci-Fi"],
+                    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"]
+                },
+                {
+                    id: 2,
+                    title: "The Shawshank Redemption",
+                    releaseYear: 1994,
+                    plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                    genres: ["Drama"],
+                    actors: ["Tim Robbins", "Morgan Freeman"]
+                },
+                {
+                    id: 3,
+                    title: "Pulp Fiction",
+                    releaseYear: 1994,
+                    plot: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+                    genres: ["Crime", "Drama"],
+                    actors: ["John Travolta", "Samuel L. Jackson"]
+                },
+                {
+                    id: 4,
+                    title: "The Dark Knight",
+                    releaseYear: 2008,
+                    plot: "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
+                    genres: ["Action", "Crime", "Drama"],
+                    actors: ["Christian Bale", "Heath Ledger"]
+                }
+            ]
+        }
+        this.addUser = this.addUser.bind(this)
+    }
     render() {
         return (<div>
             <Header title="User list"></Header>
             <div className="container">
                 <aside>
-                    <AddUser></AddUser>
+                    <AddUser onAdd={this.addUser}></AddUser>
 
                 </aside>
                 <main>
-                    <Users></Users>
+                    <Users users={this.state.users}></Users>
                 </main>
             </div>
 
         </div>)
+    }
+
+    addUser(user){
+        const id = this.state.users.length + 1
+        this.setState({users: [...this.state.users, {id, ...user}] })
+        // this.setState
+        
     }
 }
 export default App
--- src/components/addUser.js
+++ src/components/addUser.js
@@ -1,15 +1,34 @@
 import React from "react"
 
 class AddUser extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: "",
+            releaseYear: "",
+            plot: "",
+            genres: "",
+            actors: ""
+        }
+    }
+
+
     render() {
         return (
             <form>
-                <input placeholder="Title" type="text"></input>
-                <input placeholder="Release Year" type="number"></input>
-                <textarea placeholder="Plot" type="text"></textarea>
-                <input placeholder="Genres" id="genres" type="text"></input>
-                <input placeholder="Actors" id="actors" type="text"></input>
-                <button type="button">Add</button>
+                <input placeholder="Title" type="text" onChange={(e) => this.setState({ title: e.target.value })}></input>
+                <input placeholder="Release Year" type="number" onChange={(e) => this.setState({ releaseYear: e.target.value })}></input>
+                <textarea placeholder="Plot" type="text" onChange={(e) => this.setState({ plot: e.target.value })}></textarea>
+                <input placeholder="Genres" id="genres" type="text" onChange={(e) => this.setState({ genres: e.target.value })}></input>
+                <input placeholder="Actors" id="actors" type="text" onChange={(e) => this.setState({ actors: e.target.value })}></input>
+                <button type="button" onClick={() => this.props.onAdd({
+                    title: this.state.title,
+                    releaseYear: this.state.releaseYear,
+                    plot: this.state.plot,
+                    genres: this.state.genres.split(",").map(genre => genre.trim()),
+                    actors: this.state.actors.split(",").map(actor => actor.trim())
+
+                })}>Add</button>
 
             </form>
         )
