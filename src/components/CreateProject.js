708496eb67549139b12c5e67fc70c3dc558cf409
Artur Stassenko <artur.stassenko@ivkhk.ee>
2024-09-09 10:55:51 +0300

    обновленны зависимости устранены ошибки с форматом HTML в стилеReact

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -9,82 +9,82 @@ export class CreateProject extends Component {
                         <div className="col-md-8 col-sm-6">
                             <div className="form-floating">
                                 <input placeholder="Project title" type="text" id="project_title" className="form-control" name='project_title' />
-                                <label for="floatingSelect text-black">Works with selects</label>
+                                <label htmlFor="floatingSelect text-black">Works with selects</label>
                             </div>
                         </div>
                         <div className="col-md-4 col-sm-6">
-                            <div class="form-floating">
-                                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-                                    <option selected>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                            <div className="form-floating">
+                                <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
+                                    <option select="true">Open this select menu</option>
+                                    <option defaultValue="1">One</option>
+                                    <option defaultValue="2">Two</option>
+                                    <option defaultValue="3">Three</option>
                                 </select>
-                                <label for="floatingSelect text-black">Works with selects</label>
+                                <label htmlFor="floatingSelect text-black">Works with selects</label>
                             </div>
                         </div>
                         <div className="col-md-4 col-sm-6">
                             <div className="form-floating"><select className="form-select" id="floatingSelectPrivacy">
                                 <option>Select privacy</option>
-                                <option value="1">Data Privacy One</option>
-                                <option value="2">Data Privacy Two</option>
-                                <option value="3">Data Privacy Three</option>
-                            </select><label for="floatingSelectPrivacy">Project privacy</label></div>
+                                <option defaultValue="1">Data Privacy One</option>
+                                <option defaultValue="2">Data Privacy Two</option>
+                                <option defaultValue="3">Data Privacy Three</option>
+                            </select><label htmlFor="floatingSelectPrivacy">Project privacy</label></div>
                         </div>
                         <div className="col-md-4 col-sm-6">
                             <div className="form-floating"><select className="form-select" id="floatingSelectTeam">
                                 <option>Select team</option>
-                                <option value="1">Team One</option>
-                                <option value="2">Team Two</option>
-                                <option value="3">Team Three</option>
-                            </select><label for="floatingSelectTeam">Team</label></div>
+                                <option defaultValue="1">Team One</option>
+                                <option defaultValue="2">Team Two</option>
+                                <option defaultValue="3">Team Three</option>
+                            </select><label htmlFor="floatingSelectTeam">Team</label></div>
                         </div>
                         <div className="col-md-4 col-sm-6">
                             <div className="form-floating"><select className="form-select" id="floatingSelectAssignees">
                                 <option>Select assignees</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select><label for="floatingSelectAssignees">People</label></div>
+                                <option defaultValue="1">One</option>
+                                <option defaultValue="2">Two</option>
+                                <option defaultValue="3">Three</option>
+                            </select><label htmlFor="floatingSelectAssignees">People</label></div>
                         </div>
                         <div className="col-md-4 col-sm-6">
                             <div className="form-floating"><select className="form-select" id="floatingSelectAdmin">
                                 <option>Select admin</option>
-                                <option value="1">Data Privacy One</option>
-                                <option value="2">Data Privacy Two</option>
-                                <option value="3">Data Privacy Three</option>
-                            </select><label for="floatingSelectAdmin">Project Lead</label></div>
+                                <option defaultValue="1">Data Privacy One</option>
+                                <option defaultValue="2">Data Privacy Two</option>
+                                <option defaultValue="3">Data Privacy Three</option>
+                            </select><label htmlFor="floatingSelectAdmin">Project Lead</label></div>
                         </div>
                         <div className="col-md-4 col-sm-6">
                             <div className="form-floating"><input placeholder="Project title" type="text" id="startDate"
-                                className="form-control flatpickr-input" readonly="readonly" />
-                                <label for="startDate">Start date</label>
+                                className="form-control flatpickr-input" readOnly="readonly" />
+                                <label htmlFor="startDate">Start date</label>
                             </div>
 
                         </div>
                         <div className="col-md-4 col-sm-6">
                             <div className="form-floating"><input placeholder="Deadline" type="text" id="deadline"
-                                className="form-control flatpickr-input" readonly="readonly" />
-                                <label for="deadline">Deadline</label>
+                                className="form-control flatpickr-input" readOnly="readonly" />
+                                <label htmlFor="deadline">Deadline</label>
                             </div>
 
                         </div>
                         <div className="my-4 col-12">
                             <div className="form-floating"><textarea placeholder="Leave a comment here" id="floatingProjectOverview"
-                                className="form-control"></textarea><label for="floatingProjectOverview">Project
+                                className="form-control"></textarea><label htmlFor="floatingProjectOverview">Project
                                     overview</label></div>
                         </div>
                         <div className="my-4 col-md-6">
                             <div className="form-floating"><select className="form-select" id="floatingSelectClient">
                                 <option>Select client</option>
-                                <option value="1">Client One</option>
-                                <option value="2">Client Two</option>
-                                <option value="3">Client Three</option>
-                            </select><label for="floatingSelectClient">Client</label></div>
+                                <option defaultValue="1">Client One</option>
+                                <option defaultValue="2">Client Two</option>
+                                <option defaultValue="3">Client Three</option>
+                            </select><label htmlFor="floatingSelectClient">Client</label></div>
                         </div>
                         <div className="gy-6 col-md-6">
                             <div className="form-floating"><input placeholder="Budget" type="text" id="floatingInputBudget"
-                                className="form-control" /><label for="floatingInputBudget">Budget</label></div>
+                                className="form-control" /><label htmlFor="floatingInputBudget">Budget</label></div>
                         </div>
 
                         <div className="gy-6 col-12">
